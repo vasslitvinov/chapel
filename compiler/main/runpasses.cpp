46036826a397b43ec7f90d8352b1b04574ac0c93
Vassily Litvinov <vasslitvinov@users.noreply.github.com>
2023-05-10 15:11:19 -0700

    add pc - debugging instrumentation
    Signed-off-by: Vassily Litvinov <vasslitvinov@users.noreply.github.com>

--- compiler/main/runpasses.cpp
+++ compiler/main/runpasses.cpp
@@ -26,6 +26,7 @@
 #include "parser.h"
 #include "passes.h"
 #include "PhaseTracker.h"
+#include "AstDumpToNode.h"
 
 #include <cstdio>
 #include <sys/time.h>
@@ -184,6 +185,11 @@ static void setupStopAfterPass() {
 void runPasses(PhaseTracker& tracker) {
 
   setupLogfiles();
+  //vass - here because I am patching this file already
+  AstDumpToNode::compact = true;
+  AstDumpToNode::delimitEnter = "(";
+  AstDumpToNode::delimitExit  = ")";
+
 
   if (printPasses == true || printPassesFile != 0) {
     tracker.ReportPass();
@@ -224,6 +230,10 @@ static void runPass(PhaseTracker& tracker, size_t passIndex) {
 
   if (fPrintStatistics[0] != '\0' && passIndex > 0)
     printStatistics("clean");
+  if (::getenv("CHPL_VASS_gdb")) {
+    // announce the pass, with some info
+    printf("%9d  %s\n", lastNodeIDUsed(), info->name);
+  }
 
   (*(info->passFunction))();
 
